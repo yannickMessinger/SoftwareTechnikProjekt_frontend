10bd8825da49b3f9ef9ae8bd0be9e2cf77ac7486
Marc Bachmann <marc.bachmann@student.hs-rm.de>
2022-11-10 17:54:02 +0100

    import playerListItem into PlayerList

--- src/components/Lobby/Lobby.vue
+++ src/components/Lobby/Lobby.vue
@@ -1,14 +1,23 @@
 <!-- Component that represents overview of lobbys-->
 <template>
     <h3>LobbyComponent</h3>
-    <div>
-
+    <div class="lobbyList">
+        <LobbyList></LobbyList>
     </div>
+
+    <div class="playerList">
+        <PlayerList></PlayerList>
+    </div>
+
+    <button>Play</button>
 
     
 </template>
 
 <script setup lang = 'ts'>
+import LobbyList from './LobbyList.vue';
+import PlayerList from './PlayerList.vue';
+
 
 </script>
 
--- src/components/Lobby/PlayerList.vue
+++ src/components/Lobby/PlayerList.vue
@@ -1,6 +1,14 @@
 <!-- Component that represents a list of active players in a selected lobby-->
 <template>
     <h3>PlayerListComponent</h3>
+    <table>
+        <tbody>
+            <PlayerListItem></PlayerListItem>
+        </tbody>
+    </table>
 </template>
 
-<script setup lang = 'ts'></script>
+<script setup lang = 'ts'>
+import PlayerListItem from './PlayerListItem.vue';
+
+</script>
