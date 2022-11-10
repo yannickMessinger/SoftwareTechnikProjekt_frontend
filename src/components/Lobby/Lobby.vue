33adebcdd0ffa0f386e62b5374ed02d350526ea0
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-11-10 14:45:35 +0100

    init commit to create single Lobby Component and LobbyList Component to embed multiple Lobbys

---
+++ src/components/Lobby/Lobby.vue
@@ -0,0 +1,7 @@
+<!-- Component that represents single lobby-->
+<template>
+    <h3>LobbyComponent</h3>
+</template>
+
+<script setup lang = 'ts'></script>
+
---
+++ src/components/Lobby/LobbyList.vue
@@ -0,0 +1,6 @@
+<!-- Component that represents list of lobby's to choose from-->
+<template>
+    <h3>LobbyListComponent</h3>
+</template>
+
+<script setup lang = 'ts'></script>
