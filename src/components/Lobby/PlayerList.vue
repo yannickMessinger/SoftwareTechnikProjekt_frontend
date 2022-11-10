4f18175cc466d6e74234f1fcf141a1b7e505d58d
Marc Bachmann <marc.bachmann@student.hs-rm.de>
2022-11-10 15:13:52 +0100

    init commit for PlayerList component and PlayerListItem component

---
+++ src/components/Lobby/PlayerList.vue
@@ -0,0 +1,6 @@
+<!-- Component that represents a list of active players in a selected lobby-->
+<template>
+    <h3>PlayerListComponent</h3>
+</template>
+
+<script setup lang = 'ts'></script>
---
+++ src/components/Lobby/PlayerListItem.vue
@@ -0,0 +1,6 @@
+<!-- Component that represents a single player in the player list-->
+<template>
+    <h3>PlayerListItemComponent</h3>
+</template>
+
+<script setup lang = 'ts'></script>
