c592d885358b8b85f0d7578c9dbd249c17281513
Sean Dittmann <sean.dittmann@student.hs-rm.de>
2022-11-12 01:13:34 +0100

    added test-app and modified main.ts

---
+++ src/App-StreetplanerTestside.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import StreetListTestside from './components/streetplaner/StreetListTestside.vue'
+
+</script>
+
+<template>
+  <h1 class="title">Street List Testside</h1>
+  <br/>
+  <div class="streetList">
+    <StreetListTestside></StreetListTestside>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  color:black;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+/** import App from './App.vue'*/ /**original */
+import App from './App-StreetplanerTestside.vue' /**modified for testing */
 
 createApp(App).mount('#app')
