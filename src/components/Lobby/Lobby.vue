4599ed171a9ff4188d9cceef03268c85b4d6f767
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-11-15 14:25:33 +0100

    prepared fetch to backend display error msg formatting

--- src/components/Lobby/Lobby.vue
+++ src/components/Lobby/Lobby.vue
@@ -1,43 +1,45 @@
 <!-- Component that represents overview of lobbys-->
 <template>
-    <h3>Lobby Component</h3>
+  <h3>Lobby Component</h3>
 
-    
-        <div class="flex-container">
-            <LobbyList :liste="lobbyList.lobbylist"></LobbyList>
-            <PlayerList :liste="playerList.playerlist"></PlayerList>
-        </div>
-   
+  <div v-if="lobbyList.lobbylist.length > 0">
+    <div class="flex-container">
+      <LobbyList :liste="lobbyList.lobbylist"></LobbyList>
+      <PlayerList :liste="playerList.playerlist"></PlayerList>
+    </div>
+  </div>
+  <div v-else>
+    <p>Leider keine Lobbys vorhanden:(</p>
+  </div>
 
-    <button>Play</button>
+  <button @click="updateLobbyList()">REFRESH</button>
 
-    
+  <button>Play</button>
 </template>
 
-<script setup lang = 'ts'>
-import { useLobbyList } from '../../services/useLobbyList';
-import { usePlayerList } from '../../services/usePlayerList';
-import LobbyList from './LobbyList.vue';
-import PlayerList from './PlayerList.vue';
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useLobbyList, updateLobbyList } from "../../services/useLobbyList";
+import { usePlayerList } from "../../services/usePlayerList";
+import LobbyList from "./LobbyList.vue";
+import PlayerList from "./PlayerList.vue";
+
+onMounted(async () => {
+  //await  updateLobbyList()
+  console.log("hier update von backend");
+});
 
 const { lobbyList } = useLobbyList();
 const { playerList } = usePlayerList();
-
 </script>
 
-
 <style scoped>
-
-.flex-container{
-    display: flex;
-    align-items:flex-start;
+.flex-container {
+  display: flex;
+  align-items: flex-start;
 }
 
-h3{
-    text-align: center;
+h3 {
+  text-align: center;
 }
-
-
-
 </style>
-
--- src/components/Lobby/LobbyList.vue
+++ src/components/Lobby/LobbyList.vue
@@ -1,48 +1,29 @@
 <!-- Component that represents list of lobby's to choose from-->
 <template>
-    
-
-    
-                  <div class="flex-container">
-                  <h3>List of available lobbys:</h3>
-                  <LobbyListItem :lobby="ele" v-for="ele in liste"></LobbyListItem>
-                  </div>
-          
-
+  <div class="flex-container">
+    <h3>List of available lobbys:</h3>
+    <LobbyListItem :lobby="ele" v-for="ele in liste"></LobbyListItem>
+  </div>
 </template>
 
-<script setup lang = 'ts'>
+<script setup lang="ts">
+import LobbyListItem from "./LobbyListItem.vue";
+import { ILobbyListItem } from "../../typings/ILobbyListItem";
 
-
-import LobbyListItem from './LobbyListItem.vue';
-import { ILobbyListItem } from '../../typings/ILobbyListItem'
-
-   const props = defineProps<{
-      liste: Readonly<ILobbyListItem[]>,
-   }>()
-
-
-   
-
+const props = defineProps<{
+  liste: Readonly<ILobbyListItem[]>;
+}>();
 </script>
 
-
 <style scoped>
-
-
-
-.flex-container{
-   height: 800px;
-   width: 50%;
-   display: flex;
-   flex-direction: column;
-   align-items: start;
-   justify-content: start;
-   overflow: auto;
-   /*background-color: blue;*/
+.flex-container {
+  height: 800px;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  overflow: auto;
+  /*background-color: blue;*/
 }
-
-
 </style>
-
-
