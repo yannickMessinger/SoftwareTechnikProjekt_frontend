67508ead3b28a66c345160ddb7e47f9f728db152
Jona Simonavicius <jona.simonavicius@student.hs-rm.de>
2022-11-16 15:14:24 +0100

    chore: examples for event bus usage (for #114) feat: added eventBus.ts

---
+++ src/components/streetplaner/Test1.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    // Example for event bus, emit event
+    import useEventBus from './../../services/eventBus';
+    const {emit}=useEventBus();
+    function onClick() {
+        emit('test1-event', 5);
+    }
+</script>
+
+<template>
+    <button @click="onClick()">Klick mich</button>
+</template>
---
+++ src/components/streetplaner/Test2.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    // Example for event bus, on event
+    import { ref, watch } from 'vue';
+    import useEventBus from '../../services/eventBus';
+    const { bus } = useEventBus();
+    const x = ref(0);
+    watch(() =>  bus.value.get('test1-event'), (val) => {
+        x.value = val[0];
+    });   
+</script>
+
+<template>
+    <p>Zahl ist: {{ x }}</p>
+</template>
