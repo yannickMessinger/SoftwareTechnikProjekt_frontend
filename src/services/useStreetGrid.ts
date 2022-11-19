9576124386d81b5bba4418f8c12565be65c22443
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-11-19 15:49:58 +0100

    changed object to send

--- src/services/useStreetGrid.ts
+++ src/services/useStreetGrid.ts
@@ -1,76 +1,62 @@
-import { reactive, readonly } from "vue";
-import { IGridElement } from "./IGridElement";
+import {reactive, readonly} from "vue";
+import {IGridElement} from "./IGridElement";
 
-export function useStreetGrid( ) {
-    
-    
+export function useStreetGrid() {
+
 
     interface IGridState {
         playerGrid: IGridElement[][],
         errorMsg: string
-        
+
         //playerName: string,
         //lobby noch?? 
     }
 
 
+    const gridState = reactive<IGridState>({
+        playerGrid: Array(1).fill([]).map(() => Array(1).fill(null)),
+        errorMsg: ''
+    });
 
-    const gridState = reactive<IGridState>({playerGrid: Array(1).fill([]).map(() => Array(1).fill(null)), errorMsg:''});
 
-
-    function setGridState(setGrid:IGridElement[][]){
+    function setGridState(setGrid: IGridElement[][]) {
         console.log("setGridState");
         gridState.playerGrid = setGrid;
     }
-    
-    
 
-    async function saveStreetGrid():Promise<void> {
-        
-
+    async function saveStreetGrid(): Promise<void> {
         const url = '/api/streetgrid';
-       
-        
-        try{
 
+        try {
             const response = await fetch(url, {
                 method: 'POST',
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify(gridState.playerGrid)
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(gridState)
             })
 
-
             if (!response.ok) {
-                
                 gridState.errorMsg = response.statusText;
-                throw new Error (response.statusText);
+                throw new Error(response.statusText);
             }
 
-            gridState.errorMsg ='';
-           
+            gridState.errorMsg = '';
             console.log("saved streetgrid");
 
-        
-        }catch(error){
+        } catch (error) {
             console.log('error save streetgrid')
         }
-
-
-
-
     }
 
-
-    function showGridState(){
+    function showGridState() {
         console.log("showGridState");
-        console.log(JSON.stringify(gridState.playerGrid));
+        console.log(JSON.stringify(gridState));
     }
 
-    function parseStreetGrid(){
+    function parseStreetGrid() {
 
     }
 
-    
+
     return {
         gridState: readonly(gridState),
         saveStreetGrid,
