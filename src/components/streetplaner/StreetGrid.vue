501df7c1943731cf7ddf92eafda52b7d34934487
Jona Simonavicius <jona.simonavicius@student.hs-rm.de>
2022-11-20 12:44:31 +0100

    refactor: use ToolEnum to check selected tool refactor: moved reset grid to function

--- src/components/streetplaner/StreetGrid.vue
+++ src/components/streetplaner/StreetGrid.vue
@@ -11,7 +11,7 @@
     const toolState = reactive({ tool: ToolEnum.EMPTY, block: { texture: "" }});
 
     watch(() => bus.value.get('tool-select-event'), (val) => {
-        toolState.tool = val[0].name;
+        toolState.tool = val[0];
         console.log(toolState);
     });
     watch(() => bus.value.get('block-select-event'), (val) => {
@@ -21,12 +21,7 @@
 
     // create and initialize streetGrid
     const streetGrid: IGridElement[][] = reactive(Array(gridSizeX).fill([]).map(() => Array(gridSizeY).fill(null)));
-    // fill streetGrid with empty IGridElements
-    for (let row=0; row < gridSizeX; row++) {
-        for (let col=0; col < gridSizeY; col++) {
-            streetGrid[row][col] = { id: -1, posX: row, posY: col, texture: ""};
-        }
-    }
+    resetGrid();
 
     // initialize gridSize
     const gridSize = ref(40);
@@ -37,18 +32,27 @@
     // onClick handles click on specific cell
     function onClick(cell: any) {
         // set texture of clicked cell to dummy
-        if (toolState.tool == "create") {
+        if (toolState.tool == ToolEnum.CREATE) {
             streetGrid[cell.posX][cell.posY].texture = toolState.block.texture;
         }
     }
 
     // onMouseMove sets texture to all cells over which the mouse is moved while the mouse button is pressed
     function onMouseMove(cell: any, event: any) {
-        if (event.buttons === 1 && toolState.tool == "create") {  
+        if (event.buttons === 1 && toolState.tool == ToolEnum.CREATE) {  
             streetGrid[cell.posX][cell.posY].texture = toolState.block.texture;
         }
     }
     
+    function resetGrid() {
+        // fill streetGrid with empty IGridElements
+        for(let row=0; row<streetGrid.length; row++) {
+            for(let col=0; col<streetGrid[0].length; col++) {
+                streetGrid[row][col] = { id: -1, posX: row, posY: col, texture: ""};
+            }
+        }
+    }
+
     // disable right click context menu
     window.addEventListener('contextmenu', function (e) {
         e.preventDefault();
