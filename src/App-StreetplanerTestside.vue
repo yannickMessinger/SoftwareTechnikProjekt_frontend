048a33014a08755b8f9418961bc18bfcab2c7267
Sean Dittmann <sean.dittmann@student.hs-rm.de>
2022-11-20 13:04:01 +0100

    #34 - reconfigure structure

--- src/App-StreetplanerTestside.vue
+++ src/App-StreetplanerTestside.vue
@@ -1,16 +1,36 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import StreetListTestside from './components/streetplaner/StreetListTestside.vue'
-import StreetPlanerListComponent from './components/streetplaner/StreetPlanerListComponent.vue'
+import ListToolsComponent from './components/streetplaner/ListToolsComponent.vue'
+import ListBlocksComponent from './components/streetplaner/ListBlocksComponent.vue'
+import SelectedBlockComponent from './components/streetplaner/SelectedBlockComponent.vue'
+import SelectedToolComponent from './components/streetplaner/SelectedToolComponent.vue'
 </script>
 
 <template>
   <h1 class="title">Street List Testside</h1>
   <br/>
   <div class="streetList">
-    <StreetListTestside></StreetListTestside>
-    <StreetPlanerListComponent></StreetPlanerListComponent>
+    <table class="tableTest">
+        <tr>
+            <td>
+                <!--display tool list-->
+                <ListToolsComponent></ListToolsComponent>  
+            </td>
+            <td>
+                <!--display block list-->
+                <ListBlocksComponent></ListBlocksComponent>
+            </td>
+            <td>
+              <!--display selected tool-->
+               <SelectedToolComponent></SelectedToolComponent> 
+            </td>
+            <td>
+                <!--display selected block-->
+                <SelectedBlockComponent></SelectedBlockComponent>
+            </td>
+        </tr>
+    </table>
   </div>
 </template>
 
@@ -41,5 +61,15 @@ import StreetPlanerListComponent from './components/streetplaner/StreetPlanerLis
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-
+/** style for test table*/
+.tableTest{
+        width: 100%;
+        height: 100%;
+        display: inline-table;
+        user-select: none;
+        -webkit-user-drag: none; 
+        -khtml-user-drag: none; 
+        -moz-user-drag: none; 
+        -o-user-drag: none;
+    }
 </style>
