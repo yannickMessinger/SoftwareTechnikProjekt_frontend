f7b77d0677122b523be85ecbd22e561b79413c5a
Dennie Walo <dennie.walo@student.hs-rm.de>
2022-11-25 21:16:17 +0100

    chore: add Model examples to gameview

--- src/components/3D/GameView.vue
+++ src/components/3D/GameView.vue
@@ -1,10 +1,10 @@
 <script lang="ts">
-import { PointLight, Box, Camera, Renderer, Scene, LambertMaterial } from 'troisjs';
+import { PointLight, Box, Camera, Renderer, Scene, LambertMaterial, GltfModel, AmbientLight } from 'troisjs';
 import { defineComponent, onBeforeMount, onBeforeUnmount, onMounted, ref } from 'vue';
 import { FirstPersonCamera } from '../../models/FirstPersonCamera'
 
 export default defineComponent({
-  components: { Box, Camera, Renderer, Scene, PointLight, LambertMaterial },
+  components: { Box, Camera, Renderer, Scene, PointLight, LambertMaterial, GltfModel, AmbientLight },
 
   setup() {
     const renderer = ref();
@@ -33,10 +33,10 @@ export default defineComponent({
     <Camera ref="camera" :position="{ x:0, y:0, z: 0 }" :look-at="{x: 0, y:0, z:-1}">
     </Camera>
     <Scene background="#4DBA87">
-      <PointLight :position="{ y: 50, z: 50 }" />
-      <Box ref="box" :position="{x: 0, y:0, z:-5}">
-        <LambertMaterial />
-      </Box>
+      <AmbientLight></AmbientLight>
+      <GltfModel src="/../../../src/assets/3D_Models/Building/Haus.gltf" :position="{x: 0, y:0, z:-15}" :scale="{x: 0.4, y:0.4, z:0.4}"/>
+      <GltfModel src="/../../../src/assets/3D_Models/Building/Haus - Hoch.gltf" :position="{x: -5, y:0, z:-15}" :scale="{x: 0.4, y:0.4, z:0.4}"/>
+      <GltfModel src="/../../../src/assets/3D_Models/Building/Markt.gltf" :position="{x: 5, y:0, z:-15}" :scale="{x: 0.4, y:0.4, z:0.4}"/>
     </Scene>
   </Renderer>
 </template>
