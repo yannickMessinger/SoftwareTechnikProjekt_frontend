af93013a70358038867634c4a28bf561da43c466
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-11-26 11:32:21 +0100

    changed config to old values due to cors error

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-
+import LobbyView from './views/LobbyView.vue';
 </script>
 
 <template>
