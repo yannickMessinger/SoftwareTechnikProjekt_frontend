c039dcc269c7269210beade793e44ee64abc3101
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-11-26 11:22:56 +0100

    changed vue config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server:{
-    proxy:{
-      '/api':'http://localhost:8080',
-      '/websock': {
-        target:'ws://localhost:8080',
-        ws:true
-      }
-    }
-  }
+  plugins: [vue()]
 },)
