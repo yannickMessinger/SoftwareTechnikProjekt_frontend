c37b8e9aa6a6f24ba93d85b500a12b5be72dd187
Norbert Balaz <norbert.balaz@student.hs-rm.de>
2022-11-30 12:40:06 +0100

    Adds missing color und delets old MenuBar Comp.

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
     --woe-blue-30: #64a2ff;
     --woe-blue-50: #0b8aff;
     --woe-blue-60: #005ff8;
+    --woe-blue-70: #004ac2;
     --woe-blue-80: #07224c;
     --woe-blue-90: #081b39;
     --woe-black: #0a0a09;
@@ -37,6 +38,7 @@
     --woe-green-30: #63ffa1;
     --woe-green-50: #00d36e;
     --woe-green-60: #04ac51;
+    --woe-green-70: #048c41;
     --woe-green-80: #09381f;
     --woe-green-90: #0b2918;
     --woe-red-10: #fef2f2;
@@ -44,6 +46,7 @@
     --woe-red-30: #ff5e5e;
     --woe-red-50: #f52626;
     --woe-red-60: #eb0000;
+    --woe-red-70: #bc0202;
     --woe-red-80: #400807;
     --woe-red-90: #2e0808;
     --woe-yellow-10: #fffadd;
@@ -51,6 +54,7 @@
     --woe-yellow-30: #ffed72;
     --woe-yellow-50: #ffdf00;
     --woe-yellow-60: #fc0;
+    --woe-yellow-70: rgb(235, 188, 2);
     --woe-yellow-80: #4f340d;
     --woe-yellow-90: #2f1a07;
     --woe-orange-10: #fef7f2;
@@ -58,6 +62,7 @@
     --woe-orange-30: #ffb47d;
     --woe-orange-50: #ff8831;
     --woe-orange-60: #f06500;
+    --woe-orange-70: #dd5d01;
     --woe-orange-80: #802800;
     --woe-orange-90: #461600;
     --woe-violet-10: #f5f2ff;
@@ -68,14 +73,14 @@
     --woe-violet-80: #29005a;
     --woe-violet-90: #1a0033;
 
-    --btn-primary: var(--woe-green-50);
-    --btn-primary-hover: var(--woe-green-60);
-    --btn-secondary: var(--woe-blue-50);
-    --btn-secondary-hover: var(--woe-blue-60);
-    --btn-tertiary: var(--woe-gray-50);
-    --btn-teriary-hover: var(--woe-gray-60);
-    --btn-cancle: var(--woe-red-50);
-    --btn-cancle-hover: var(--woe-red-60);
+    --btn-primary: var(--woe-green-60);
+    --btn-primary-hover: var(--woe-green-70);
+    --btn-secondary: var(--woe-blue-60);
+    --btn-secondary-hover: var(--woe-blue-70);
+    --btn-tertiary: var(--woe-gray-60);
+    --btn-teriary-hover: var(--woe-gray-70);
+    --btn-cancle: var(--woe-red-60);
+    --btn-cancle-hover: var(--woe-red-70);
 
     font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
     font-size: 16px;
