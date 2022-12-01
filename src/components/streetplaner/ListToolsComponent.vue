7ef4674d574bf921852427940eb97eac5e15fc3a
Jona Simonavicius <jona.simonavicius@student.hs-rm.de>
2022-12-01 12:13:31 +0100

    fix: fixed bug where blocks were deleted when in create mode refactor: removed debug console logs

--- src/components/streetplaner/ListToolsComponent.vue
+++ src/components/streetplaner/ListToolsComponent.vue
@@ -30,11 +30,11 @@
     toolList[1] = { tool: ToolEnum.DELETE, id: 1, name: "delete", texture: (pathToPictures+"tool-icons/delete.png")};
     toolList[2] = { tool: ToolEnum.EDIT, id: 2, name: "edit", texture: (pathToPictures+"tool-icons/edit.png")};
         
-    function onToolClick(clickedTool:any){
+    function onToolClick(clickedTool: any){
         /** if the selected tool is the clicked tool, it gets deselected by restoring the default tool
          * otherwhise the clicked tool is now the selected tool.
         */
-        console.log(selectedTool.tool.name);
+        // console.log(selectedTool.tool.name);
         if(selectedTool.tool.id==clickedTool.id){
             selectedTool.tool = defaultTool;
         }else{
@@ -44,7 +44,7 @@
         emit('tool-select-event', selectedTool.tool.tool);
         /** fire a tool select event to mark a tool change for selectedToolComponent. Sends out the complete tool of the selected tool*/
         emit('tool-select-component-event', selectedTool.tool);
-        console.log(selectedTool.tool.name);
+        // console.log(selectedTool.tool.name);
     }
 </script>
 
