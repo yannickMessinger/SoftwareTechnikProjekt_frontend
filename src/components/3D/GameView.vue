6c65f08d05276344d20ec80304c70e4b0e787374
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-10 15:37:05 +0100

    init display of street elements on ground pane

--- src/components/3D/GameView.vue
+++ src/components/3D/GameView.vue
@@ -6,11 +6,6 @@ import { FirstPersonCamera } from '../../models/FirstPersonCamera';
 import { IGridElement } from '../../services/streetplaner/IGridElement';
 
 
-
-
-
-
-
 export default defineComponent({
   components: { Box, Camera, Renderer, Scene,  GltfModel, AmbientLight,Plane,PhongMaterial},
 
@@ -19,29 +14,36 @@ export default defineComponent({
     const box = ref();
     const camera = ref();
     const fpsCamera = new FirstPersonCamera(camera, box)
-    let gridSizeX = 40;
-    let gridSizeY = 40;
-    const streetGrid: IGridElement[][] = reactive(Array(gridSizeX).fill([]).map(() => Array(gridSizeY).fill(null)));
-    const fieldSize = 4;
+    let gridSizeX = 100;
+    let gridSizeY = 100;
+    let m=10
+    let n = 10
+    const streetGrid: IGridElement[][] = Array(m).fill([]).map(() => Array(n).fill(null));
+    const fieldSize = 10;
     const buildingIDMap = new Map().set('haus','/../../../src/assets/3D_Models/Building/Markt.gltf');
     let path:string = '/../../../src/assets/3D_Models/Building/Markt.gltf';
-    let intersection_path = '/../../../src/assets/3D_Models/Streets/intersection_road.gltf'
-    let testArr:any = []
+    let intersection_path = '/../../../src/assets/3D_Models/Streets/straight_road.gltf'
+    
+
+    
   
-    function initTestArray(){
-      
-      for(let i=0; i<10; i++){
-        testArr.push({x:i,z:0, path:intersection_path})
+    function initStreetGrid(){
+     
+      for(let row=0; row<streetGrid.length; row++) {
+            for(let col=0; col<streetGrid[0].length; col++) {
+              streetGrid[row][col] = {id:0, posX:row, posY:col, rotation: 0, texture:intersection_path};
+            }
+        
       }
+
+      
       
     }
 
-    initTestArray();
+    initStreetGrid();
 
 
     function calcCoordinateX(n:number){
-      console.log(`n:${n}`)
-      console.log((gridSizeX * (-0.5)) + (n * fieldSize) + (fieldSize / 2))
       return (gridSizeX * (-0.5)) + (n * fieldSize) + (fieldSize / 2);
     }
 
@@ -55,7 +57,7 @@ export default defineComponent({
       });
     })
 
-    console.log(testArr);
+   
 
     return {
       renderer,
@@ -66,8 +68,7 @@ export default defineComponent({
       calcCoordinateX,
       calcCoordinateZ,
       buildingIDMap,
-      path,
-      testArr
+      path
     }
   }
 });
@@ -82,18 +83,17 @@ export default defineComponent({
      <!-- <GltfModel src="/../../../src/assets/3D_Models/Building/Haus.gltf" :position="{x: 0, y:0, z:0}" :scale="{x: 0.4, y:0.4, z:0.4}"/>-->
       <!--<GltfModel src="/../../../src/assets/3D_Models/Building/Haus - Hoch.gltf" :position="{x: -5, y:0, z:-15}" :scale="{x: 0.4, y:0.4, z:0.4}"/>-->
       <!-- <GltfModel src="/../../../src/assets/3D_Models/Building/Markt.gltf" :position="{x: 5, y:0, z:-15}" :scale="{x: 0.4, y:0.4, z:0.4}"/>-->
-      <Plane :width="40" :height="40" :rotation="{x: -Math.PI /2}" :position="{x:0, y:0, z:0}" receive-shadow>
+      <Plane :width="100" :height="100" :rotation="{x: -Math.PI /2}" :position="{x:0, y:0, z:0}" receive-shadow>
         <PhongMaterial color="#999999" :props="{ depthWrite: false }" /></Plane>
       
         
-        <div v-for = "building in testArr">
+        <div v-for = "row in streetGrid">
+          <div v-for ="column in row">
             
-              <!--<Plane :width="4" :height="4" :rotation="{x: -Math.PI /2}" :position="{x:calcCoordinateX(n), y:0, z:-18}" receive-shadow>
-              <PhongMaterial color="#FF0000" :props="{ depthWrite: false }" /></Plane>-->
-              <GltfModel v-bind:src="building.path" :position="{x:calcCoordinateX(building.x), y:0.5, z: calcCoordinateZ(building.z)}" :scale="{x: 0.05, y:0.05, z:0.05}" :rotation="{x:0, y:0, z:0}"/>
-              
+            <GltfModel v-bind:src="column.texture" :position="{x:calcCoordinateX(column.posX), y:0, z: calcCoordinateZ(column.posY)}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:0, z:0}"/>
+          </div>    
         </div>
-        
+      
 
         
 
