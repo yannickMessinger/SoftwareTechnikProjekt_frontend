71e1e246bf8d05fee8be1edb3128fd7af9894b4d
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-11 14:53:20 +0100

    possible to place different street elements connection to backend

--- src/components/3D/GameView.vue
+++ src/components/3D/GameView.vue
@@ -4,6 +4,10 @@ import { PointLight, Box, Camera, Renderer, Scene, LambertMaterial, GltfModel, A
 import { defineComponent, onBeforeMount, onBeforeUnmount, onMounted, reactive, ref } from 'vue';
 import { FirstPersonCamera } from '../../models/FirstPersonCamera';
 import { IGridElement } from '../../services/streetplaner/IGridElement';
+import { useStreetGridList, updateStreetGridList } from '../../services/streetplaner/useStreetGridList';
+import { StreetGridDTO } from '../../services/streetplaner/StreetGridDTO';
+import { useLobbyList } from '../../services/useLobbyList';
+import { IStreetElement } from '../../services/streetplaner/IStreetElement';
 
 
 export default defineComponent({
@@ -21,26 +25,26 @@ export default defineComponent({
     let m=10
     let n = 10
     /*Array of Buildings and Streets passed from 2D Planner*/
-    const streetGrid: IGridElement[][] = Array(m).fill([]).map(() => Array(n).fill(null));
+    
     const fieldSize = 10;
     /*Map of 3d-model paths*/
-    const buildingIDMap = new Map().set('haus','/../../../src/assets/3D_Models/Building/Markt.gltf');
-    let path:string = '/../../../src/assets/3D_Models/Building/Markt.gltf';
-    let intersection_path = '/../../../src/assets/3D_Models/Streets/straight_road.gltf'
-    
+    const buildingIDMap = new Map()
+    buildingIDMap.set(0,'/../../../src/assets/3D_Models/Streets/straight_road.gltf');
+    buildingIDMap.set(1,'/../../../src/assets/3D_Models/Streets/intersection_road.gltf');
+    buildingIDMap.set(2,'/../../../src/assets/3D_Models/Streets/curved_road.gltf');
 
+    let building:string = '/../../../src/assets/3D_Models/Building/Markt.gltf';
+    
+    let mapElements:IStreetElement[] = Array();
+    const lobbyState = useLobbyList().activeLobbyState;
+
+    mapElements = useStreetGridList().streetGridDTO.mapObjects;
+      updateStreetGridList(lobbyState.mapID).then(() => {
+        console.log(mapElements);
+        });
     
   
-    function initStreetGrid(){
-     
-      for(let row=0; row<streetGrid.length; row++) {
-            for(let col=0; col<streetGrid[0].length; col++) {
-              streetGrid[row][col] = {id:0, posX:row, posY:col, rotation: 0, texture:intersection_path};
-            } 
-      }
-    }
-
-    initStreetGrid();
+    
 
     /*Models position are saved from the Backend counting from 0 upwards.
       x:0, z:0 describes the upper left corner. On a 100 x 100 Field the lower right corner would be x:99, z: 99.
@@ -49,32 +53,42 @@ export default defineComponent({
 
     /*Calculates X coordinates position of loaded Model */
     function calcCoordinateX(n:number){
+      console.log((gridSizeX * (-0.5)) + (n * fieldSize) + (fieldSize / 2))
       return (gridSizeX * (-0.5)) + (n * fieldSize) + (fieldSize / 2);
     }
 
     /*Calculates Z coordinates position of loaded Model */
     function calcCoordinateZ(n:number){
+      console.log((gridSizeY * (-0.5)) + (n * fieldSize) + (fieldSize / 2))
       return (gridSizeY * (-0.5)) + (n * fieldSize) + (fieldSize / 2);
     }
 
     onMounted(() => {
+
+      /*
+      mapElements = useStreetGridList().streetGridDTO.mapObjects;
+      updateStreetGridList(lobbyState.mapID).then(() => {
+        console.log(mapElements);
+        });
+      */
+
       renderer.value.onBeforeRender(() => {
         fpsCamera.update();
       });
+     
     })
 
-   
+    
 
     return {
       renderer,
       camera,
       box,
       fpsCamera,
-      streetGrid,
       calcCoordinateX,
       calcCoordinateZ,
       buildingIDMap,
-      path
+      mapElements
     }
   }
 });
@@ -94,12 +108,12 @@ export default defineComponent({
       
         
         <!--All elements placed in the editor are read from the list and placed in the scene-->
-        <div v-for = "row in streetGrid">
-          <div v-for ="column in row">
+        <div v-for = "ele in mapElements">
+          
             
-            <GltfModel v-bind:src="column.texture" :position="{x:calcCoordinateX(column.posX), y:0, z: calcCoordinateZ(column.posY)}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:0, z:0}"/>
+            <GltfModel v-bind:src="buildingIDMap.get(ele.objectTypeId)" :position="{x:calcCoordinateX(ele.x), y:0, z: calcCoordinateZ(ele.y)}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:0, z:0}"/>
           </div>    
-        </div>
+        
       
 
         
