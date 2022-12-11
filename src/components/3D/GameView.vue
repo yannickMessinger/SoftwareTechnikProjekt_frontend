3eab72a974f525ac2a191679e605a4b9c84410f9
Marc Bachmann <marc.bachmann@student.hs-rm.de>
2022-12-11 13:40:59 +0100

    commented 3d model place functions

--- src/components/3D/GameView.vue
+++ src/components/3D/GameView.vue
@@ -14,12 +14,16 @@ export default defineComponent({
     const box = ref();
     const camera = ref();
     const fpsCamera = new FirstPersonCamera(camera, box)
+    /*Defines the Grid Size in length by the number ob fields*/ 
     let gridSizeX = 100;
+    /*Defines the Grid Size in height by the number ob fields*/ 
     let gridSizeY = 100;
     let m=10
     let n = 10
+    /*Array of Buildings and Streets passed from 2D Planner*/
     const streetGrid: IGridElement[][] = Array(m).fill([]).map(() => Array(n).fill(null));
     const fieldSize = 10;
+    /*Map of 3d-model paths*/
     const buildingIDMap = new Map().set('haus','/../../../src/assets/3D_Models/Building/Markt.gltf');
     let path:string = '/../../../src/assets/3D_Models/Building/Markt.gltf';
     let intersection_path = '/../../../src/assets/3D_Models/Streets/straight_road.gltf'
@@ -32,21 +36,23 @@ export default defineComponent({
       for(let row=0; row<streetGrid.length; row++) {
             for(let col=0; col<streetGrid[0].length; col++) {
               streetGrid[row][col] = {id:0, posX:row, posY:col, rotation: 0, texture:intersection_path};
-            }
-        
+            } 
       }
-
-      
-      
     }
 
     initStreetGrid();
 
+    /*Models position are saved from the Backend counting from 0 upwards.
+      x:0, z:0 describes the upper left corner. On a 100 x 100 Field the lower right corner would be x:99, z: 99.
+      On the 3d Game View the coordinates x:0, z:0 describes the center of our Grid. The upper left corner would be x:-50, z:-50.
+      The following two methods calculate the Models position bades on the backend memory structure and adapts it to the frontend structure.*/
 
+    /*Calculates X coordinates position of loaded Model */
     function calcCoordinateX(n:number){
       return (gridSizeX * (-0.5)) + (n * fieldSize) + (fieldSize / 2);
     }
 
+    /*Calculates Z coordinates position of loaded Model */
     function calcCoordinateZ(n:number){
       return (gridSizeY * (-0.5)) + (n * fieldSize) + (fieldSize / 2);
     }
@@ -87,6 +93,7 @@ export default defineComponent({
         <PhongMaterial color="#999999" :props="{ depthWrite: false }" /></Plane>
       
         
+        <!--All elements placed in the editor are read from the list and placed in the scene-->
         <div v-for = "row in streetGrid">
           <div v-for ="column in row">
             
