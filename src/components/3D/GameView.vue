6fea2e62d2b0fffedffea8486e0bdea16ab696af
Marc Bachmann <marc.bachmann@student.hs-rm.de>
2022-12-18 15:51:22 +0100

    enviroment addition

--- src/components/3D/GameView.vue
+++ src/components/3D/GameView.vue
@@ -34,8 +34,12 @@ export default defineComponent({
     /*Map of 3d-model paths*/
     const buildingIDMap = new Map()
     buildingIDMap.set(0,'/../../../src/assets/3D_Models/Streets/straight_road.gltf');
-    buildingIDMap.set(1,'/../../../src/assets/3D_Models/Building/Markt.gltf');
-    
+    buildingIDMap.set(1,'/../../../src/assets/3D_Models/Streets/curved_road.gltf');
+    buildingIDMap.set(2,'/../../../src/assets/3D_Models/Streets/intersection_road.gltf');
+    buildingIDMap.set(17, '/../../../src/assets/3D_Models/Enviroment/enviroment_1.gltf');
+    buildingIDMap.set(18, '/../../../src/assets/3D_Models/Enviroment/enviroment_2.gltf');
+    buildingIDMap.set(19, '/../../../src/assets/3D_Models/Enviroment/enviroment_3.gltf');
+    buildingIDMap.set(20, '/../../../src/assets/3D_Models/Enviroment/enviroment_4.gltf');
 
     /*Riadians is used to rotate Models. The following map set the radians for the passed rotation value from backend*/
     const rotationMap = new Map()
@@ -49,7 +53,6 @@ export default defineComponent({
     rotationMap.set(3, 3*Math.PI/2)
 
     
-
     
     //useStreetGridList().createDummyList();
     //console.log( useStreetGridList().createDummyList())
@@ -57,12 +60,15 @@ export default defineComponent({
     //useStreetGridList().createDummyList();
     resetMapEles()
     
-    const mapElements = computed(() => useStreetGridList().gameMap.gameMapObjects);
    
     //const mapElements = computed(() => useStreetGridList().createDummyList());
     const lobbyState = activeLobby;
     //console.log("INIT MAP ELES");
     //console.log(mapElements);
+    const enviroment = makeEnviroment()
+    const mapElements = computed(() => useStreetGridList().streetGridDTO.mapObjects);
+    console.log("INIT MAP ELES");
+    console.log(mapElements);
     
     
   
@@ -70,6 +76,40 @@ export default defineComponent({
        useStreetGridList().resetMapEles();
        useStreetGridList().resetGameMap();
     }
+
+    /*
+    function makeFinalArray(){
+      const finalElements : Array<IStreetElement>= [];
+        for(let i = 0; i < <any>mapElements[1].length; i++){
+          finalElements[mapElements[i].x] = ele
+        }
+        for(let i = 0; i < gridSizeY/fieldSize; i++){
+          for(let j = 0; j < gridSizeX/fieldSize; j++){
+          }
+        }
+    }
+    */
+    
+
+    function makeEnviroment(){
+      const enviroElements : Array<IMapObject>= [];
+        let counter = 0;
+      for(let i = 0; i < gridSizeY/fieldSize; i++){
+        for(let j = 0; j < gridSizeX/fieldSize; j++){
+          enviroElements[counter] = {objectTypeId:randomNumer(17,20), x: i, y: j, rotation:randomNumer(0,3)}
+          counter +=1;
+          //console.log("count: " + counter + " i: " + i + " j: " + j);
+        }
+        
+      }
+      return enviroElements;
+    }
+    /*Generates a random number between given numbers (min and max included)
+      Used du randomly select between 4 diffrent enviroment modules and for choosing random rotation for enviroment modules */
+    function randomNumer(min: number, max: number) { // min and max included 
+      return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
 
     /*Models position are saved from the Backend counting from 0 upwards.
       x:0, z:0 describes the upper left corner. On a 100 x 100 Field the lower right corner would be x:99, z: 99.
@@ -116,7 +156,8 @@ export default defineComponent({
       mapElements,
       rotationMap,
       gridSizeX,
-      gridSizeY
+      gridSizeY,
+      enviroment
     }
   }
 });
@@ -126,14 +167,19 @@ export default defineComponent({
   <Renderer resize="window" ref="renderer">
     <Camera ref="camera" :position="{ x:0, y:0, z: 0 }" :look-at="{x: 0, y:0, z:-1}">
     </Camera>
-    <Scene background="#4DBA87">
+    <Scene background="#87CEEB">
       <AmbientLight></AmbientLight>
       <Plane :width=gridSizeX :height=gridSizeY :rotation="{x: -Math.PI /2}" :position="{x:0, y:0, z:0}" receive-shadow>
         <PhongMaterial color="#999999" :props="{ depthWrite: false }" /></Plane>
       
        <!--  <GltfModel src='/../../../src/assets/3D_Models/Streets/straight_road_rotated.gltf' :position="{x:0, y:0, z:45}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:0, z:0}"/>-->
-        
-        <!--All elements placed in the editor are read from the list and placed in the scene-->
+       <GltfModel src="/../../../src/assets/3D_Models/Enviroment/enviroment_kachel_2.gltf" :position="{x:5, y:0, z: 5}" :scale="{x: 0.5, y:0.5, z:0.5}"/>
+
+       <div v-for="ele in enviroment">
+        <GltfModel v-bind:src="buildingIDMap.get(ele.objectTypeId)" :position="{x:calcCoordinateX(ele.y), y:0, z: calcCoordinateZ(ele.x)}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:rotationMap.get(ele.rotation), z:0}"/>
+       </div>
+
+        <!-- All elements placed in the editor are read from the list and placed in the scene-->
         <div v-for = "ele in mapElements">
           <GltfModel v-bind:src="buildingIDMap.get(ele.objectTypeId)" :position="{x:calcCoordinateX(ele.y), y:0, z: calcCoordinateZ(ele.x)}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:rotationMap.get(ele.rotation), z:0}"/>
         </div>    
