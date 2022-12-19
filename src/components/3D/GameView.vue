43ad5a6062c8033c46fd4f87fc3db3f548bb9959
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-19 10:42:03 +0100

    formatting

--- src/components/3D/GameView.vue
+++ src/components/3D/GameView.vue
@@ -1,65 +1,105 @@
 <script lang="ts">
-import { PointLight, Box, Camera, Renderer, Scene, LambertMaterial, GltfModel, AmbientLight,Plane,PhongMaterial } from 'troisjs';
-import { computed, defineComponent, onBeforeMount, onBeforeUnmount, onMounted, reactive, ref } from 'vue';
-import { FirstPersonCamera } from '../../models/FirstPersonCamera';
-import { useGameView } from '../../services/3DGameView/useGameView';
-
-
-
+import {
+  PointLight,
+  Box,
+  Camera,
+  Renderer,
+  Scene,
+  LambertMaterial,
+  GltfModel,
+  AmbientLight,
+  Plane,
+  PhongMaterial,
+} from "troisjs";
+import {
+  computed,
+  defineComponent,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  reactive,
+  ref,
+} from "vue";
+import { FirstPersonCamera } from "../../models/FirstPersonCamera";
+import { useGameView } from "../../services/3DGameView/useGameView";
 
 export default defineComponent({
-  components: { Box, Camera, Renderer, Scene,  GltfModel, AmbientLight,Plane,PhongMaterial},
+  components: {
+    Box,
+    Camera,
+    Renderer,
+    Scene,
+    GltfModel,
+    AmbientLight,
+    Plane,
+    PhongMaterial,
+  },
 
   setup() {
     const renderer = ref();
     const box = ref();
     const camera = ref();
-    const fpsCamera = new FirstPersonCamera(camera, box)
-   
-    /*Defines the Grid Size in length by the number ob fields*/ 
+    const fpsCamera = new FirstPersonCamera(camera, box);
+
+    /*Defines the Grid Size in length by the number ob fields*/
     let gridSizeX = 100;
-    /*Defines the Grid Size in height by the number ob fields*/ 
+    /*Defines the Grid Size in height by the number ob fields*/
     let gridSizeY = 100;
 
-    let m=10
-    let n = 10
+    let m = 10;
+    let n = 10;
 
-    useGameView().setMapWidthAndMapHeight(m,n);
+    useGameView().setMapWidthAndMapHeight(m, n);
     /*Array of Buildings and Streets passed from 2D Planner*/
-    
+
     const fieldSize = 10;
     /*Map of 3d-model paths*/
-    const buildingIDMap = new Map()
-    buildingIDMap.set(0,'/../../../src/assets/3D_Models/Streets/straight_road.gltf');
-    buildingIDMap.set(1,'/../../../src/assets/3D_Models/Streets/curved_road.gltf');
-    buildingIDMap.set(2,'/../../../src/assets/3D_Models/Streets/intersection_road.gltf');
-    buildingIDMap.set(4,'/../../../src/assets/3D_Models/Building/Haus.gltf');
+    const buildingIDMap = new Map();
+    buildingIDMap.set(
+      0,
+      "/../../../src/assets/3D_Models/Streets/straight_road.gltf"
+    );
+    buildingIDMap.set(
+      1,
+      "/../../../src/assets/3D_Models/Streets/curved_road.gltf"
+    );
+    buildingIDMap.set(
+      2,
+      "/../../../src/assets/3D_Models/Streets/intersection_road.gltf"
+    );
+    buildingIDMap.set(4, "/../../../src/assets/3D_Models/Building/Haus.gltf");
 
-    buildingIDMap.set(17, '/../../../src/assets/3D_Models/Enviroment/enviroment_1.gltf');
-    buildingIDMap.set(18, '/../../../src/assets/3D_Models/Enviroment/enviroment_2.gltf');
-    buildingIDMap.set(19, '/../../../src/assets/3D_Models/Enviroment/enviroment_3.gltf');
-    buildingIDMap.set(20, '/../../../src/assets/3D_Models/Enviroment/enviroment_4.gltf');
+    buildingIDMap.set(
+      17,
+      "/../../../src/assets/3D_Models/Enviroment/enviroment_1.gltf"
+    );
+    buildingIDMap.set(
+      18,
+      "/../../../src/assets/3D_Models/Enviroment/enviroment_2.gltf"
+    );
+    buildingIDMap.set(
+      19,
+      "/../../../src/assets/3D_Models/Enviroment/enviroment_3.gltf"
+    );
+    buildingIDMap.set(
+      20,
+      "/../../../src/assets/3D_Models/Enviroment/enviroment_4.gltf"
+    );
 
     /*Riadians is used to rotate Models. The following map set the radians for the passed rotation value from backend*/
-    const rotationMap = new Map()
+    const rotationMap = new Map();
     /*No rotation*/
-    rotationMap.set(0, 0)
+    rotationMap.set(0, 0);
     /*90 degree rotation*/
-    rotationMap.set(1, Math.PI/2)
+    rotationMap.set(1, Math.PI / 2);
     /*180 degree rotation*/
-    rotationMap.set(2, Math.PI)
+    rotationMap.set(2, Math.PI);
     /*270 degree rotation*/
-    rotationMap.set(3, 3*Math.PI/2)
+    rotationMap.set(3, (3 * Math.PI) / 2);
 
-    
-    
-    
-    useGameView().resetGameMapObjects();    
-    
+    useGameView().resetGameMapObjects();
+
     const mapElements = computed(() => useGameView().gameState.gameMapObjects);
-    
-
-
 
     /*Models position are saved from the Backend counting from 0 upwards.
       x:0, z:0 describes the upper left corner. On a 100 x 100 Field the lower right corner would be x:99, z: 99.
@@ -67,33 +107,24 @@ export default defineComponent({
       The following two methods calculate the Models position bades on the backend memory structure and adapts it to the frontend structure.*/
 
     /*Calculates X coordinates position of loaded Model */
-    function calcCoordinateX(n:number){
+    function calcCoordinateX(n: number) {
       //console.log((gridSizeX * (-0.5)) + (n * fieldSize) + (fieldSize / 2))
-      return (gridSizeX * (-0.5)) + (n * fieldSize) + (fieldSize / 2);
+      return gridSizeX * -0.5 + n * fieldSize + fieldSize / 2;
     }
 
     /*Calculates Z coordinates position of loaded Model */
-    function calcCoordinateZ(n:number){
+    function calcCoordinateZ(n: number) {
       //console.log((gridSizeY * (-0.5)) + (n * fieldSize) + (fieldSize / 2))
-      return (gridSizeY * (-0.5)) + (n * fieldSize) + (fieldSize / 2);
+      return gridSizeY * -0.5 + n * fieldSize + fieldSize / 2;
     }
 
     onMounted(() => {
-
-      
-      
-      
       useGameView().updateMapObjsFromGameState();
-     
-    
 
       renderer.value.onBeforeRender(() => {
         fpsCamera.update();
       });
-     
-    })
-
-    
+    });
 
     return {
       renderer,
@@ -107,39 +138,50 @@ export default defineComponent({
       rotationMap,
       gridSizeX,
       gridSizeY,
-      //enviroment
-    }
-  }
+    };
+  },
 });
 </script>
 
 <template>
   <Renderer resize="window" ref="renderer">
-    <Camera ref="camera" :position="{ x:0, y:0, z: 0 }" :look-at="{x: 0, y:0, z:-1}">
+    <Camera
+      ref="camera"
+      :position="{ x: 0, y: 0, z: 0 }"
+      :look-at="{ x: 0, y: 0, z: -1 }"
+    >
     </Camera>
     <Scene background="#87CEEB">
       <AmbientLight></AmbientLight>
-      <Plane :width=gridSizeX :height=gridSizeY :rotation="{x: -Math.PI /2}" :position="{x:0, y:0, z:0}" receive-shadow>
-        <PhongMaterial color="#999999" :props="{ depthWrite: false }" /></Plane>
-      
-       <!--  <GltfModel src='/../../../src/assets/3D_Models/Streets/straight_road_rotated.gltf' :position="{x:0, y:0, z:45}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:0, z:0}"/>-->
+      <Plane
+        :width="gridSizeX"
+        :height="gridSizeY"
+        :rotation="{ x: -Math.PI / 2 }"
+        :position="{ x: 0, y: 0, z: 0 }"
+        receive-shadow
+      >
+        <PhongMaterial color="#999999" :props="{ depthWrite: false }"
+      /></Plane>
 
-       <!--<div v-for="ele in enviroment">
+      <!--  <GltfModel src='/../../../src/assets/3D_Models/Streets/straight_road_rotated.gltf' :position="{x:0, y:0, z:45}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:0, z:0}"/>-->
+
+      <!--<div v-for="ele in enviroment">
         <GltfModel v-bind:src="buildingIDMap.get(ele.objectTypeId)" :position="{x:calcCoordinateX(ele.y), y:0, z: calcCoordinateZ(ele.x)}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:rotationMap.get(ele.rotation), z:0}"/>
        </div>-->
 
-        <!-- All elements placed in the editor are read from the list and placed in the scene-->
-        <div v-for = "ele in mapElements">
-          <GltfModel v-bind:src="buildingIDMap.get(ele.objectTypeId)" :position="{x:calcCoordinateX(ele.y), y:0, z: calcCoordinateZ(ele.x)}" :scale="{x: 0.5, y:0.5, z:0.5}" :rotation="{x:0, y:rotationMap.get(ele.rotation), z:0}"/>
-        </div>    
-        
-      
-
-        
-
-      
-     
-    
+      <!-- All elements placed in the editor are read from the list and placed in the scene-->
+      <div v-for="ele in mapElements">
+        <GltfModel
+          v-bind:src="buildingIDMap.get(ele.objectTypeId)"
+          :position="{
+            x: calcCoordinateX(ele.y),
+            y: 0,
+            z: calcCoordinateZ(ele.x),
+          }"
+          :scale="{ x: 0.5, y: 0.5, z: 0.5 }"
+          :rotation="{ x: 0, y: rotationMap.get(ele.rotation), z: 0 }"
+        />
+      </div>
     </Scene>
   </Renderer>
 </template>
--- src/services/3DGameView/useGameView.ts
+++ src/services/3DGameView/useGameView.ts
@@ -1,81 +1,82 @@
 import { reactive, ref } from "vue";
 import { IMapObject } from "../streetplaner/IMapObject";
 import useUser from "../UserStore";
-const {activeLobby} = useUser();
+const { activeLobby } = useUser();
 const mapWidth = ref();
 const mapHeight = ref();
 
-const gameState = reactive({ gameMapObjects: Array<IMapObject>(), mapObjsFromBackEnd:  Array<IMapObject>(), init: false});
+const gameState = reactive({
+  gameMapObjects: Array<IMapObject>(),
+  mapObjsFromBackEnd: Array<IMapObject>(),
+  init: false,
+});
 
-
-export function setMapWidthAndMapHeight(width:number, height:number){
-    mapWidth.value = width;
-    mapHeight.value = height;
+export function setMapWidthAndMapHeight(width: number, height: number) {
+  mapWidth.value = width;
+  mapHeight.value = height;
 }
 
-
 export function useGameView() {
-    return {
-        gameState,
-        resetGameMapObjects,
-        resetMapObjsFromBackEnd,
-        fillGameState,
-        updateMapObjsFromGameState,
-        setMapWidthAndMapHeight
-
-    }
+  return {
+    gameState,
+    resetGameMapObjects,
+    resetMapObjsFromBackEnd,
+    fillGameState,
+    updateMapObjsFromGameState,
+    setMapWidthAndMapHeight,
+  };
 }
 
 export async function updateMapObjsFromGameState() {
-    const url = "/api/map/objects";
-    const mapId = activeLobby.value.mapId;
+  const url = "/api/map/objects";
+  const mapId = activeLobby.value.mapId;
 
-    try {
-        if (mapId === -1) { throw new Error("mapID is -1, make sure you are in a lobby."); }
-        const response = await fetch(`${url}/${mapId}`, { method: "GET" });
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        const jsondata = await response.json();
-        gameState.mapObjsFromBackEnd = jsondata;
-        fillGameState()
-    } catch (error: any) {
-        console.log(error.statusText);
-        let json = '{"mapObjects":[{"objectTypeId":1,"x":2,"y":9,"rotation":0},{"objectTypeId":0,"x":2,"y":10,"rotation":1},{"objectTypeId":0,"x":2,"y":11,"rotation":1},{"objectTypeId":0,"x":2,"y":12,"rotation":1},{"objectTypeId":0,"x":2,"y":13,"rotation":1},{"objectTypeId":0,"x":2,"y":14,"rotation":1},{"objectTypeId":1,"x":2,"y":15,"rotation":1},{"objectTypeId":0,"x":3,"y":9,"rotation":0},{"objectTypeId":6,"x":3,"y":14,"rotation":0},{"objectTypeId":0,"x":3,"y":15,"rotation":2},{"objectTypeId":0,"x":4,"y":9,"rotation":0},{"objectTypeId":1,"x":4,"y":12,"rotation":0},{"objectTypeId":0,"x":4,"y":13,"rotation":1},{"objectTypeId":0,"x":4,"y":14,"rotation":1},{"objectTypeId":1,"x":4,"y":15,"rotation":2},{"objectTypeId":0,"x":5,"y":9,"rotation":0},{"objectTypeId":0,"x":5,"y":12,"rotation":2},{"objectTypeId":0,"x":6,"y":9,"rotation":0},{"objectTypeId":5,"x":6,"y":10,"rotation":0},{"objectTypeId":0,"x":6,"y":12,"rotation":2},{"objectTypeId":1,"x":7,"y":5,"rotation":0},{"objectTypeId":0,"x":7,"y":6,"rotation":1},{"objectTypeId":0,"x":7,"y":7,"rotation":1},{"objectTypeId":0,"x":7,"y":8,"rotation":1},{"objectTypeId":2,"x":7,"y":9,"rotation":0},{"objectTypeId":0,"x":7,"y":10,"rotation":1},{"objectTypeId":0,"x":7,"y":11,"rotation":1},{"objectTypeId":1,"x":7,"y":12,"rotation":2},{"objectTypeId":1,"x":7,"y":14,"rotation":0},{"objectTypeId":0,"x":7,"y":15,"rotation":1},{"objectTypeId":0,"x":7,"y":16,"rotation":1},{"objectTypeId":1,"x":7,"y":17,"rotation":1},{"objectTypeId":0,"x":8,"y":5,"rotation":2},{"objectTypeId":3,"x":8,"y":8,"rotation":0},{"objectTypeId":0,"x":8,"y":9,"rotation":2},{"objectTypeId":3,"x":8,"y":10,"rotation":0},{"objectTypeId":0,"x":8,"y":14,"rotation":2},{"objectTypeId":0,"x":8,"y":17,"rotation":0},{"objectTypeId":0,"x":9,"y":5,"rotation":2},{"objectTypeId":4,"x":9,"y":8,"rotation":0},{"objectTypeId":0,"x":9,"y":9,"rotation":2},{"objectTypeId":3,"x":9,"y":10,"rotation":0},{"objectTypeId":0,"x":9,"y":14,"rotation":2},{"objectTypeId":0,"x":9,"y":17,"rotation":0},{"objectTypeId":0,"x":10,"y":5,"rotation":2},{"objectTypeId":4,"x":10,"y":8,"rotation":0},{"objectTypeId":0,"x":10,"y":9,"rotation":2},{"objectTypeId":0,"x":10,"y":14,"rotation":2},{"objectTypeId":0,"x":10,"y":17,"rotation":0},{"objectTypeId":1,"x":11,"y":5,"rotation":3},{"objectTypeId":0,"x":11,"y":6,"rotation":1},{"objectTypeId":0,"x":11,"y":7,"rotation":1},{"objectTypeId":0,"x":11,"y":8,"rotation":1},{"objectTypeId":2,"x":11,"y":9,"rotation":0},{"objectTypeId":0,"x":11,"y":10,"rotation":1},{"objectTypeId":0,"x":11,"y":11,"rotation":1},{"objectTypeId":0,"x":11,"y":12,"rotation":1},{"objectTypeId":0,"x":11,"y":13,"rotation":1},{"objectTypeId":2,"x":11,"y":14,"rotation":0},{"objectTypeId":0,"x":11,"y":15,"rotation":1},{"objectTypeId":0,"x":11,"y":16,"rotation":1},{"objectTypeId":1,"x":11,"y":17,"rotation":2},{"objectTypeId":0,"x":12,"y":9,"rotation":0},{"objectTypeId":0,"x":12,"y":14,"rotation":0},{"objectTypeId":0,"x":13,"y":9,"rotation":0},{"objectTypeId":0,"x":13,"y":14,"rotation":0},{"objectTypeId":1,"x":14,"y":9,"rotation":3},{"objectTypeId":0,"x":14,"y":10,"rotation":1},{"objectTypeId":0,"x":14,"y":11,"rotation":1},{"objectTypeId":0,"x":14,"y":12,"rotation":1},{"objectTypeId":0,"x":14,"y":13,"rotation":1},{"objectTypeId":1,"x":14,"y":14,"rotation":2}]}';
-        gameState.mapObjsFromBackEnd = JSON.parse(json).mapObjects;
+  try {
+    if (mapId === -1) {
+      throw new Error("mapID is -1, make sure you are in a lobby.");
     }
-}
-
-
-export function resetGameMapObjects(){
-    gameState.gameMapObjects.splice(0, gameState.gameMapObjects.length);
-   
-    
-}
-
-export function resetMapObjsFromBackEnd(){
-    gameState.mapObjsFromBackEnd.splice(0, gameState.mapObjsFromBackEnd.length);
-   
-}
-
-function randomNumer(min: number, max: number) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    const response = await fetch(`${url}/${mapId}`, { method: "GET" });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const jsondata = await response.json();
+    gameState.mapObjsFromBackEnd = jsondata;
+    fillGameState();
+  } catch (error: any) {
+    console.log(error.statusText);
+    let json =
+      '{"mapObjects":[{"objectTypeId":1,"x":2,"y":9,"rotation":0},{"objectTypeId":0,"x":2,"y":10,"rotation":1},{"objectTypeId":0,"x":2,"y":11,"rotation":1},{"objectTypeId":0,"x":2,"y":12,"rotation":1},{"objectTypeId":0,"x":2,"y":13,"rotation":1},{"objectTypeId":0,"x":2,"y":14,"rotation":1},{"objectTypeId":1,"x":2,"y":15,"rotation":1},{"objectTypeId":0,"x":3,"y":9,"rotation":0},{"objectTypeId":6,"x":3,"y":14,"rotation":0},{"objectTypeId":0,"x":3,"y":15,"rotation":2},{"objectTypeId":0,"x":4,"y":9,"rotation":0},{"objectTypeId":1,"x":4,"y":12,"rotation":0},{"objectTypeId":0,"x":4,"y":13,"rotation":1},{"objectTypeId":0,"x":4,"y":14,"rotation":1},{"objectTypeId":1,"x":4,"y":15,"rotation":2},{"objectTypeId":0,"x":5,"y":9,"rotation":0},{"objectTypeId":0,"x":5,"y":12,"rotation":2},{"objectTypeId":0,"x":6,"y":9,"rotation":0},{"objectTypeId":5,"x":6,"y":10,"rotation":0},{"objectTypeId":0,"x":6,"y":12,"rotation":2},{"objectTypeId":1,"x":7,"y":5,"rotation":0},{"objectTypeId":0,"x":7,"y":6,"rotation":1},{"objectTypeId":0,"x":7,"y":7,"rotation":1},{"objectTypeId":0,"x":7,"y":8,"rotation":1},{"objectTypeId":2,"x":7,"y":9,"rotation":0},{"objectTypeId":0,"x":7,"y":10,"rotation":1},{"objectTypeId":0,"x":7,"y":11,"rotation":1},{"objectTypeId":1,"x":7,"y":12,"rotation":2},{"objectTypeId":1,"x":7,"y":14,"rotation":0},{"objectTypeId":0,"x":7,"y":15,"rotation":1},{"objectTypeId":0,"x":7,"y":16,"rotation":1},{"objectTypeId":1,"x":7,"y":17,"rotation":1},{"objectTypeId":0,"x":8,"y":5,"rotation":2},{"objectTypeId":3,"x":8,"y":8,"rotation":0},{"objectTypeId":0,"x":8,"y":9,"rotation":2},{"objectTypeId":3,"x":8,"y":10,"rotation":0},{"objectTypeId":0,"x":8,"y":14,"rotation":2},{"objectTypeId":0,"x":8,"y":17,"rotation":0},{"objectTypeId":0,"x":9,"y":5,"rotation":2},{"objectTypeId":4,"x":9,"y":8,"rotation":0},{"objectTypeId":0,"x":9,"y":9,"rotation":2},{"objectTypeId":3,"x":9,"y":10,"rotation":0},{"objectTypeId":0,"x":9,"y":14,"rotation":2},{"objectTypeId":0,"x":9,"y":17,"rotation":0},{"objectTypeId":0,"x":10,"y":5,"rotation":2},{"objectTypeId":4,"x":10,"y":8,"rotation":0},{"objectTypeId":0,"x":10,"y":9,"rotation":2},{"objectTypeId":0,"x":10,"y":14,"rotation":2},{"objectTypeId":0,"x":10,"y":17,"rotation":0},{"objectTypeId":1,"x":11,"y":5,"rotation":3},{"objectTypeId":0,"x":11,"y":6,"rotation":1},{"objectTypeId":0,"x":11,"y":7,"rotation":1},{"objectTypeId":0,"x":11,"y":8,"rotation":1},{"objectTypeId":2,"x":11,"y":9,"rotation":0},{"objectTypeId":0,"x":11,"y":10,"rotation":1},{"objectTypeId":0,"x":11,"y":11,"rotation":1},{"objectTypeId":0,"x":11,"y":12,"rotation":1},{"objectTypeId":0,"x":11,"y":13,"rotation":1},{"objectTypeId":2,"x":11,"y":14,"rotation":0},{"objectTypeId":0,"x":11,"y":15,"rotation":1},{"objectTypeId":0,"x":11,"y":16,"rotation":1},{"objectTypeId":1,"x":11,"y":17,"rotation":2},{"objectTypeId":0,"x":12,"y":9,"rotation":0},{"objectTypeId":0,"x":12,"y":14,"rotation":0},{"objectTypeId":0,"x":13,"y":9,"rotation":0},{"objectTypeId":0,"x":13,"y":14,"rotation":0},{"objectTypeId":1,"x":14,"y":9,"rotation":3},{"objectTypeId":0,"x":14,"y":10,"rotation":1},{"objectTypeId":0,"x":14,"y":11,"rotation":1},{"objectTypeId":0,"x":14,"y":12,"rotation":1},{"objectTypeId":0,"x":14,"y":13,"rotation":1},{"objectTypeId":1,"x":14,"y":14,"rotation":2}]}';
+    gameState.mapObjsFromBackEnd = JSON.parse(json).mapObjects;
   }
-  
-export function fillGameState():void{
-    
-    let counter = 0;
-    for(let i = 0; i < mapWidth.value; i++){
-        for (let j = 0; j < mapHeight.value; j++){
-            
-            gameState.gameMapObjects[counter]  = {objectTypeId:randomNumer(17,20), x: i, y: j, rotation:randomNumer(0,3)}
-            counter +=1;
-        }
-    }
-   
-        gameState.mapObjsFromBackEnd.forEach((mapObj) => { 
-            gameState.gameMapObjects[(mapObj.x*10) + mapObj.y] = mapObj
-        })
+}
 
-    
+export function resetGameMapObjects() {
+  gameState.gameMapObjects.splice(0, gameState.gameMapObjects.length);
+}
+
+export function resetMapObjsFromBackEnd() {
+  gameState.mapObjsFromBackEnd.splice(0, gameState.mapObjsFromBackEnd.length);
+}
+
+function randomNumer(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function fillGameState(): void {
+  let counter = 0;
+  for (let i = 0; i < mapWidth.value; i++) {
+    for (let j = 0; j < mapHeight.value; j++) {
+      gameState.gameMapObjects[counter] = {
+        objectTypeId: randomNumer(17, 20),
+        x: i,
+        y: j,
+        rotation: randomNumer(0, 3),
+      };
+      counter += 1;
+    }
+  }
+
+  gameState.mapObjsFromBackEnd.forEach((mapObj) => {
+    gameState.gameMapObjects[mapObj.x * 10 + mapObj.y] = mapObj;
+  });
 }
