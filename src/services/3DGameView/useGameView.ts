c437574c410576d4cf5a77812283cf73f8f65e52
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-21 10:55:58 +0100

    docu

--- src/services/3DGameView/useGameView.ts
+++ src/services/3DGameView/useGameView.ts
@@ -5,11 +5,24 @@ const { activeLobby } = useUser();
 const mapWidth = ref();
 const mapHeight = ref();
 
+/**
+ * Gamestate obj with two lists containing necessary MapObjects
+ * @param gameMapObjects : complete List of mapobjects, that is passed to Gameview to be rendered. 
+ * @param mapObjsFromBackEnd : list of mapobjects that have been placed in Editor, fetched from backend. List gameMapObjects is updated with these values
+ * 
+ */
 const gameState = reactive({
   gameMapObjects: Array<IMapObject>(),
   mapObjsFromBackEnd: Array<IMapObject>(),
   init: false,
 });
+
+/**
+ * 
+ * @param width sets counter variable for loop of function fillGameState
+ * @param height sets counter variable for loop of function fillGameState
+ * coming from GameView
+ */
 
 export function setMapWidthAndMapHeight(width: number, height: number) {
   mapWidth.value = width;
@@ -27,6 +40,10 @@ export function useGameView() {
   };
 }
 
+/**
+ * function to fetch MapObjects from backend that have been placed in MapEditor. Is then used to update gamestate.gameMapObjects. 
+ * If fetch fails predefined list is set. 
+ */
 export async function updateMapObjsFromGameState() {
   const url = "/api/map/objects";
   const mapId = activeLobby.value.mapId;
@@ -50,18 +67,34 @@ export async function updateMapObjsFromGameState() {
   }
 }
 
+/**
+ * resets the gameMapObject list
+ */
 export function resetGameMapObjects() {
   gameState.gameMapObjects.splice(0, gameState.gameMapObjects.length);
 }
 
+/**
+ * resets the mapObjectsFromBackend list
+ */
 export function resetMapObjsFromBackEnd() {
   gameState.mapObjsFromBackEnd.splice(0, gameState.mapObjsFromBackEnd.length);
 }
 
+/**
+ * function to generate random numbers between the given min and max values
+ * @param min min value for random number
+ * @param max value for random numbwer
+ * @returns random number
+ */
 function randomNumer(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
+/**
+ * first fills gameMapObjects list with dummy environment elements with random rotation.
+ * then for each element of the mapObject from backend, the corresponding index in the gameMapObject list is calculated and the element 
+ * on this index gets replaced with the object from backend.  
+ */
 export function fillGameState(): void {
   let counter = 0;
   for (let i = 0; i < mapWidth.value; i++) {
