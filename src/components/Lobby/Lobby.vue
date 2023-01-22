ea3617ad5ef53a6e4e7f6bfd3eb5fcf4beffad9c
Florian Bohn <florian.bohn@student.hs-rm.de>
2023-01-22 22:21:11 +0100

    Updated chat: It is now possible to differentiate between global and lobby chat.

--- src/components/Lobby/Lobby.vue
+++ src/components/Lobby/Lobby.vue
@@ -15,111 +15,106 @@
 </template>
 
 <script setup lang="ts">
-    import { ILobby } from "../../typings/ILobby"
-    import BasicButton from "../Buttons/BasicButton.vue"
-    import useUser from "../../services/UserStore"
-    import router from "../../router/router"
-    import { useLobbyList } from "../../services/useLobbyList"
-    import { onMounted } from "vue"
-    import { useChat } from "../../services/Chat/useChat"
+import { ILobby } from "../../typings/ILobby"
+import BasicButton from "../Buttons/BasicButton.vue"
+import useUser from "../../services/UserStore"
+import router from "../../router/router"
+import { useLobbyList } from "../../services/useLobbyList"
+import { onMounted } from "vue"
+import { useChat } from "../../services/Chat/useChat"
 
-    const props = defineProps<{
-        lobby: ILobby
-    }>()
+const props = defineProps<{
+    lobby: ILobby
+}>()
 
-    const { setActiveLobby, name } = useUser()
-    const { receiveLobbyUpdates, joinMessage } = useLobbyList()
-    const { updateActiveChatLobbyId } = useChat(name.value)
+const { setActiveLobby, name } = useUser()
+const { receiveLobbyUpdates, joinMessage } = useLobbyList()
+//const { updateActiveChatLobbyId } = useChat(name.value, )
 
-    //for later purposes to link to selected lobby via Vue Router
-    async function selectLobby() {
-        //set ActiveLoppy property to the selected Lobby
-        setActiveLobby(props.lobby)
+//for later purposes to link to selected lobby via Vue Router
+async function selectLobby() {
+    //set ActiveLoppy property to the selected Lobby
+    setActiveLobby(props.lobby)
 
-        //updates lobbyid for chat
-        //updateActiveChatLobbyId(props.lobby.lobbyId)
-
-        //fires JOINED event to backend to trigger persistence operations and inform other players on channel and update data
-        joinMessage()
-        router.push({
-            path: "/lobbyview",
-        })
-    }
-
-    onMounted(() => {
-        //activate websockets connection to listen for incoming updates
-        receiveLobbyUpdates()
+    //fires JOINED event to backend to trigger persistence operations and inform other players on channel and update data
+    joinMessage()
+    router.push({
+        path: "/lobbyview",
     })
+}
+
+onMounted(() => {
+    //activate websockets connection to listen for incoming updates
+    receiveLobbyUpdates()
+})
 </script>
 
 <style scoped>
-    * {
-        font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
-            "Helvetica Neue", sans-serif;
-        font-size: 16px;
-        font-weight: 500;
-        line-height: 20px;
-    }
-    * {
-        font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
-            "Helvetica Neue", sans-serif;
-        font-size: 16px;
-        font-weight: 500;
-        line-height: 20px;
-    }
+* {
+    font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 20px;
+}
+* {
+    font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 20px;
+}
 
-    button {
-        border: none;
-        border-radius: 8px;
-        cursor: pointer;
-        font-weight: 300;
-        padding: 13px 23px;
-        background-color: var(--woe-blue-60);
-        color: white;
-    }
-    button {
-        border: none;
-        border-radius: 8px;
-        cursor: pointer;
-        font-weight: 300;
-        padding: 13px 23px;
-        background-color: var(--woe-blue-60);
-        color: white;
-    }
+button {
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    font-weight: 300;
+    padding: 13px 23px;
+    background-color: var(--woe-blue-60);
+    color: white;
+}
+button {
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    font-weight: 300;
+    padding: 13px 23px;
+    background-color: var(--woe-blue-60);
+    color: white;
+}
 
-    button:hover {
-        background-color: var(--woe-blue-70);
-    }
-    button:hover {
-        background-color: var(--woe-blue-70);
-    }
+button:hover {
+    background-color: var(--woe-blue-70);
+}
+button:hover {
+    background-color: var(--woe-blue-70);
+}
 
-    .deleteButton {
-        background: var(--woe-gray-50);
-        border: none;
-        border-radius: 8px;
-        opacity: 1;
-    }
-    .deleteButton {
-        background: var(--woe-gray-50);
-        border: none;
-        border-radius: 8px;
-        opacity: 1;
-    }
+.deleteButton {
+    background: var(--woe-gray-50);
+    border: none;
+    border-radius: 8px;
+    opacity: 1;
+}
+.deleteButton {
+    background: var(--woe-gray-50);
+    border: none;
+    border-radius: 8px;
+    opacity: 1;
+}
 
-    .deleteButton:hover {
-        background-color: var(--woe-red-70);
-    }
-    .deleteButton:hover {
-        background-color: var(--woe-red-70);
-    }
+.deleteButton:hover {
+    background-color: var(--woe-red-70);
+}
+.deleteButton:hover {
+    background-color: var(--woe-red-70);
+}
 
-    td {
-        padding-bottom: 20px;
-        padding-left: 30px;
-    }
-    td {
-        padding-bottom: 20px;
-        padding-left: 30px;
-    }
+td {
+    padding-bottom: 20px;
+    padding-left: 30px;
+}
+td {
+    padding-bottom: 20px;
+    padding-left: 30px;
+}
 </style>
